5297a08fcb1a09db5cca5837fc5258bb6dcee736
Gaojianli <823805669hehe@gmail.com>
2020-04-20 11:14:07 +0800

    5th

---
+++ 5th/app/src/main/java/com/byted/chapter5/UserResponse.java
@@ -0,0 +1,12 @@
+package com.byted.chapter5;
+
+import com.google.gson.annotations.SerializedName;
+
+class UserResponse {
+    @SerializedName("errorCode")
+    public int errorCode;
+    @SerializedName("errorMsg")
+    public String errorMsg;
+    @SerializedName("data")
+    User user;
+}
